90173203f13c45d3431cd4c68e439ff9aa8699b3
idanielmeza <idanielmezaledezma@gmail.com>
2022-04-15 12:14:05 -0500

    Semana 1 terminada

---
+++ weekly_mission_1/example_10/pokemon.js
@@ -0,0 +1,15 @@
+class Pokemon{
+    constructor(name){
+        this.name = name; 
+    }
+
+    sayHello(){
+        console.log(`Hi, I'm ${this.name}!`);
+    }
+
+    sayMessage(message){
+        console.log(`${this.name} says: ${message}`);
+    }
+}
+
+export default Pokemon;
